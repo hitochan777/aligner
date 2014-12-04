74f59eecbf5a7282485cf4edcc6d8488a1bcafef
Hitoshi Otsuki <hitochan777@gmail.com>
2014-12-04 10:46:50 +0900

    remove unnecessary file

--- assess.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-base=~/Research/kftt-alignments
-
-for i in {0..4}
-do
-$base/bin/measure-alignment-error.pl $base/data/align-test.txt $base/symmetric-$1_$2_$i.txt > $base/symmetric-$1_$2_$i.acc
-$base/bin/measure-alignment-error.pl $base/data/align-test.txt $base/alignment_$1_$2_$i.txt > $base/alignment_$1_$2_$i.acc
-done
